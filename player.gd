32562599cb15f67bcd1698330fa5d4974557c28f
Janelle Tan <my.janelle.tan@gmail.com>
2024-07-26 04:04:36 +0800

    walking character and green field

---
+++ player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var speed = 50
+
+func _physics_process(delta):
+	var motion = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.play("sidewalk")
+		$AnimatedSprite2D.flip_h = true
+		motion.x = speed
+	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.play("sidewalk")
+		$AnimatedSprite2D.flip_h = false
+		motion.x = -speed
+	elif Input.is_action_pressed("ui_down"):
+		$AnimatedSprite2D.play("downwalk")
+		motion.y = speed
+	elif Input.is_action_pressed("ui_up"):
+		$AnimatedSprite2D.play("upwalk")
+		motion.y = -speed
+	else:
+		$AnimatedSprite2D.play("idle")
+	
+	velocity = motion
+	move_and_slide()
